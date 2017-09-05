082cf52de6e491d1f7b8864b896d13726919f940
developer915by <30619913+developer915by@users.noreply.github.com>
2017-09-05 15:44:05 +0300

    mane scene

---
+++ sphereinspace/Assets/scipts/Buttons.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+	
+	public Sprite layer_blue, layer_red;
+
+	void OnMouseDown () {
+		GetComponent <SpriteRenderer> ().sprite = layer_red;
+	}
+
+	void OnMouseUp () {
+		GetComponent <SpriteRenderer> ().sprite = layer_blue;
+	}
+
+	void OnMouseUpAsButton () {
+		switch (gameObject.name) {
+		case "Play":
+			SceneManager.LoadScene ("play");
+			break;
+		case "Rating":
+			Application.OpenURL ("http://google.com");
+			break;
+		}
+	}
+}
---
+++ sphereinspace/Assets/scipts/Sphere.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sphere : MonoBehaviour {
+
+	public float spead, tilt;
+	public Vector3 target;
+	void Update ()
+	{
+		transform.position = Vector3.MoveTowards (transform.position, target, Time.deltaTime * spead);	
+		if (transform.position == target && target.y != 1.5f )
+			target.y = 1.5f;
+       
+       
+        else if (transform.position == target && target.y == 1.5f)
+			target.y = 3.5f;
+        
+
+        transform.Rotate (Vector3.one * Time.deltaTime * tilt);
+	}
+}
