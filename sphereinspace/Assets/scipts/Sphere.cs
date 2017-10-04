3e5c085739a18a832788a14ce74ff8c4470b9349
developer915by <30619913+developer915by@users.noreply.github.com>
2017-10-04 21:37:19 +0300

    develop

--- sphereinspace/Assets/scipts/Sphere.cs
+++ sphereinspace/Assets/scipts/Sphere.cs
@@ -5,15 +5,18 @@ public class Sphere : MonoBehaviour {
 
 	public float spead, tilt;
 	public Vector3 target;
-	void Update ()
-	{
-		transform.position = Vector3.MoveTowards (transform.position, target, Time.deltaTime * spead);	
-		if (transform.position == target && target.y != 1.5f )
-			target.y = 1.5f;
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * spead);
+        if  (transform.position == target && target.x != -3.5f)
+            target.x = -3.5f;
+
+      
+
        
        
-        else if (transform.position == target && target.y == 1.5f)
-			target.y = 3.5f;
+        else if (transform.position == target && target.x == -3.5f)
+			target.x = 3.5f;
         
 
         transform.Rotate (Vector3.one * Time.deltaTime * tilt);
---
+++ sphereinspace/Assets/scipts/lalal.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using CnControls;
+using UnityEngine.UI;
+
+public class lalal : MonoBehaviour {
+	public Text PointText;
+	private int PointInt = 0;
+	public float titl;
+	Vector3 position;
+	void Start () {
+
+	}
+
+	void FixedUpdate () {
+		position = new Vector3(CnInputManager.GetAxis("Horizontal"), CnInputManager.GetAxis("Vertical"), 0f);
+
+		transform.position += position*Time.deltaTime*5;
+		transform.Rotate (Vector3.one * Time.deltaTime * titl);
+
+		PointText.text = PointInt.ToString ();
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		PointInt++;
+		Destroy (collider.gameObject);
+	}
+}
